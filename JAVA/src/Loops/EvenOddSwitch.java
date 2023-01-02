520c808f9fdfaa5748c69e5c89572f78bbe5f034
Aftab <aftabmulani001@gmail.com>
2023-01-02 13:57:21 +0530

    modified changes

--- JAVA/src/Loops/EvenOddSwitch.java
+++ JAVA/src/Loops/EvenOddSwitch.java
@@ -3,14 +3,14 @@ import java.util.Scanner;
 public class EvenOddSwitch {
     public static void main(String[] args) {
         Scanner obj = new Scanner(System.in);
-        System.out.print("enter nymber to check prime or not:");
+        System.out.print("enter nymber to check number even or odd:");
         int a = obj.nextInt();
         switch (a % 2) {
             case 0:
-                System.out.println(a + " is prime number");
+                System.out.println(a + " is even number");
                 break;
             case 1:
-            System.out.println(a + " is not prime number");
+            System.out.println(a + " is odd number");
             default:
                 break;
         }
